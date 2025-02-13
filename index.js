1e702de2d3e3759979c4bd298262b7ce11788d1f
space-ghost24 <144731705+space-ghost24@users.noreply.github.com>
2025-02-13 16:21:24 -0600

    Removed unnecessary comments

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//FUNCTION to get computer choice
-//WRITE the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
 function getComputerChoice(){
     let compChoice = Math.floor(Math.random() * 3);
     if (compChoice === 0){
@@ -10,12 +8,6 @@ function getComputerChoice(){
         return "scissors"
     }
 }
-
-//TEST function 
-//console.log(getComputerChoice())
-
-//FUNCTION to get human choice
-//WRITE a function that takes the user choice and returns it.
 
 function getHumanChoice(){
     let humanChoice = "";
@@ -29,42 +21,6 @@ function getHumanChoice(){
     }
     return humanChoice;
 }
-
-//TEST function
-// console.log(getHumanChoice())
-
-//DECLARE player and computer score variables and set to zero
-
-// let humanScore = 0;
-// let computerScore = 0;
-
-//FUNCTION that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
-
-// function playRound(humanChoice, computerChoice){
-//     let message = "";
-//     if (humanChoice === computerChoice){
-//         message = `Tie game! You both chose ${humanChoice}!`;
-//         console.log(message);
-//     }else if((humanChoice === "rock" && computerChoice === "scissors") || 
-//              (humanChoice === "paper" && computerChoice === "rock") || 
-//              (humanChoice === "scissors" && computerChoice === "paper"))
-//     {
-//         humanScore ++;
-//         message = `You win! ${humanChoice} beats ${computerChoice}!`;
-//         console.log(message);
-//     }else{
-//         computerScore ++;
-//         message = `You lose! ${computerChoice} beats ${humanChoice}!`; 
-//         console.log(message);       
-//     }
-// }
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humanSelection, computerSelection);
-
-//FUNCTION to play game consisting of 5 rounds
 
 function playGame(){
     let humanScore = 0;
